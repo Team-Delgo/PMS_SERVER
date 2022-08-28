7657f3305bc675322ebb20b36f61778882c9a335
Gupuroom <cjsrnr1114@gmail.com>
2022-08-28 15:22:33 +0900

    DB 변경 요소 적용

--- src/main/java/com/pms/domain/user/UserSocial.java
+++ src/main/java/com/pms/domain/user/UserSocial.java
@@ -1,5 +1,19 @@
 package com.pms.domain.user;
 
 public enum UserSocial {
-    K, N, A
+    D("Delgo"),
+    K("Kakao"),
+    N("Naver"),
+    A("Apple");
+
+    private final String name;
+
+    UserSocial(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
 }
